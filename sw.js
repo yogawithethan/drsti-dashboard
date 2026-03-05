73700f7de0fd8f9eb2777934689c7baefb5864cb
Ethan Hill <ethanhill@Ethans-MacBook-Air.local>
2026-03-05 13:29:22 +1300

    Add activity log section, bump cache to v27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'drsti-v26';
+var CACHE_NAME = 'drsti-v27';
 var SHELL_FILES = ['index.html', 'manifest.json', 'drsti.png', 'tcot-logo-gold.jpg'];
 
 self.addEventListener('install', function(e) {
